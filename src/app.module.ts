1e66988d8b845ed20372d00aad989fbd6460764d
katelynxue <katelyn_xue26@milton.edu>
2026-02-26 12:05:28 -0500

    worked on in class

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
 
 @Module({
-  imports: [ProductsModule],
+  imports: [ProductsModule, MongooseModule.forRoot('mongodb+srv://apple:apple@cluster0.ofnvdrh.mongodb.net/',
+    ),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
